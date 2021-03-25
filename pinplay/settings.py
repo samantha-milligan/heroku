1e3a049c1d046f651a0edb3653f04dfe9161573c
Samantha Milligan <smm885@nau.edu>
2021-03-24 20:12:53 -0700

    new

--- pinplay/settings.py
+++ pinplay/settings.py
@@ -27,9 +27,9 @@ with open('secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['radiant-anchorage-77650.herokuapp.com']
 
 
 # Application definition
