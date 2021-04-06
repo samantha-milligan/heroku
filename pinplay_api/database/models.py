7c97b48c30327c8fce80eb6f686132c69d0d1036
jmp458 <78055520+jmp458@users.noreply.github.com>
2021-04-06 15:58:48 -0700

    Update models.py

--- pinplay_api/database/models.py
+++ pinplay_api/database/models.py
@@ -20,7 +20,7 @@ class Playlist( models.Model ):
     
 
 class tracklist( models.Model ):
-    name =  models.CharField( max_length = 50 )
+    name = models.CharField( max_length = 50 )
 
     
     
